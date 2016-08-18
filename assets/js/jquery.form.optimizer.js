fc9f0361ad74b8121a5b5b8b76604e14615ddbd4
clemaister <clemgournay@hotmail.fr>
2016-08-18 18:18:54 +0900

    Updated remove

--- assets/js/jquery.form.optimizer.js
+++ assets/js/jquery.form.optimizer.js
@@ -173,12 +173,12 @@ $.fn.formOptimizer = function(settings){
                     if($field.hasClass("fo-not-empty") && $field.parents(".fo-fields").find(".fo-error[fo-field-id='"+$field.attr("fo-field-id")+"']").length==0){
                         $field.parents(".fo-fields").find(".fo-errors").append('<div class="fo-error" fo-field-id="'+$field.attr("fo-field-id")+'">'+message+'</div>');
                     }
-                    else if(!$field.hasClass("fo-not-empty")) $field.parent(".fo-fields").find(".fo-error[fo-field-id='"+$field.attr("fo-field-id")+"']").remove();
+                    else if(!$field.hasClass("fo-not-empty")) $field.parents(".fo-fields").find(".fo-error[fo-field-id='"+$field.attr("fo-field-id")+"']").remove();
                 }
             }   
         }
         else{
-            $field.parent(".fo-fields").find(".fo-error[fo-field-id='"+$field.attr("fo-field-id")+"']").remove();
+            $field.parents(".fo-fields").find(".fo-error[fo-field-id='"+$field.attr("fo-field-id")+"']").remove();
         }
 
         app.checkForm();
